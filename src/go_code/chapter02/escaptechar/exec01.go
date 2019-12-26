f34ac21178b04e2054ce841396b81195939211e5
lemon <422896102@qq.com>
2019-12-26 14:34:41 +0800

    init

---
+++ src/go_code/chapter02/escaptechar/exec01.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" //fmt包中提供格式化，输出，输入的函数
+
+func main() {
+    
+	fmt.Println("姓名\t年龄\t籍贯\t地址\njohn\t12\t河北\t北京")
+}
---
+++ src/go_code/chapter02/escaptechar/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt" //fmt包中提供格式化，输出，输入的函数
+
+func main() {
+	// 演示转义字符的使用 \t
+	fmt.Println("tom\tjack")
+	// 换行
+	fmt.Println("hello\nworld")
+	// 转义斜杠
+	fmt.Println("c:\\User\\lemon")
+	// 转义双引号
+	fmt.Println("tom说\"i love you\"")
+	// \r　回车 ，从当前行的最前面开始输出，覆盖掉以前的内容
+	fmt.Println("天龙八部飞山飞狐\r张飞")
+
+	
+}
